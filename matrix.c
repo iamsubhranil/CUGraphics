aa3f3b28769c2cfaf73242f02c60d2545fbfc4e8
Subhranil Mukherjee <subhranilmukherjee190@gmail.com>
2018-09-30 20:13:11 +0530

    fix: matrix: use 1D array internally for better perf and less mem

--- matrix.c
+++ matrix.c
@@ -8,7 +8,7 @@
 #include <stdarg.h>
 
 typedef struct Mat{
-    double **values;
+    double *values;
     int m, n;
 } Mat;
 
@@ -16,12 +16,20 @@ Mat* mat_new(int m, int n){
     Mat *mt = (Mat *)malloc(sizeof(Mat));
     mt->m = m;
     mt->n = n;
-    mt->values = (double **)malloc(sizeof(double *) * m);
-    for(int i = 0;i < m;i++){
-        mt->values[i] = (double *)malloc(sizeof(double) * n);
-        //memset(mt->values[i], 0, sizeof(double) * n);
-    }
+    mt->values = (double *)malloc(sizeof(double) * m * n);
     return mt;
+}
+
+static inline int mat_idx(Mat *m, int i, int j){
+    return (m->n * i) + j;
+}
+
+void mat_set(Mat *m1, int i, int j, double val){
+    m1->values[mat_idx(m1, i, j)] = val;
+}
+
+double mat_get(Mat *m1, int i, int j){
+    return m1->values[mat_idx(m1, i, j)];
 }
 
 void mat_fill(Mat *m1, ...){
@@ -29,25 +37,17 @@ void mat_fill(Mat *m1, ...){
     va_start(args, m1);
     for(int i = 0;i < m1->m;i++){
         for(int j = 0;j < m1->n;j++){
-            m1->values[i][j] = va_arg(args, double);
+            mat_set(m1, i, j, va_arg(args, double));
         }
     }
     va_end(args);
-}
-
-void mat_set(Mat *m1, int i, int j, double val){
-    m1->values[i][j] = val;
-}
-
-double mat_get(Mat *m1, int i, int j){
-    return m1->values[i][j];
 }
 
 void mat_print(Mat *m1){
     printf("\n");
     for(int i = 0;i < m1->m;i++){
         for(int j = 0;j < m1->n;j++){
-            printf("%3.5g", m1->values[i][j]);
+            printf("%3.5g", mat_get(m1, i, j));
         }
         printf("\n");
     }
@@ -70,9 +70,9 @@ Mat* mat_mult(Mat *m1, Mat *m2){
             //fflush(stdout);
             double sum = 0;
             for(int k = 0;k < m1->n;k++){
-                sum += (m1->values[i][k] * m2->values[k][j]);
+                sum += (mat_get(m1, i, k) * mat_get(m2, k, j));
             }
-            res->values[i][j] = sum;
+            mat_set(res, i, j, sum);
         }
     }
     return res;
@@ -89,7 +89,8 @@ Mat* mat_add(Mat *m1, Mat *m2){
     Mat* res = mat_new(m1->m, m1->n);
     for(int i = 0;i < m1->m;i++){
         for(int j = 0;j < m1->n;j++){
-            res->values[i][j] = m1->values[i][j] + m2->values[i][j];
+            mat_set(res, i, j,
+                    mat_get(m1, i, j) + mat_get(m2, i, j));
         }
     }
     return res;
@@ -106,15 +107,14 @@ Mat* mat_sub(Mat *m1, Mat *m2){
     Mat* res = mat_new(m1->m, m1->n);
     for(int i = 0;i < m1->m;i++){
         for(int j = 0;j < m1->n;j++){
-            res->values[i][j] = m1->values[i][j] - m2->values[i][j];
+            mat_set(res, i, j,
+                    mat_get(m1, i, j) - mat_get(m2, i, j));
         }
     }
     return res;
 }
 
 void mat_free(Mat *m1){
-    for(int i = 0;i < m1->m;i++)
-        free(m1->values[i]);
     free(m1->values);
     free(m1);
 }
